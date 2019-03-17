7511db4ffcf4b61bbf76923174089f4d1fb21687
Vincent Gim <vincegim@amazon.com>
2019-03-16 19:38:23 -0700

    Tour of Heroes initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+    heroes = HEROES;
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+      };
+
+    selectedHero: Hero;
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+    
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
